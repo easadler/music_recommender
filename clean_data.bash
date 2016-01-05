c77f50921eb07f0497dbadd90d81163169683c51
Evan Sadler <easadler@gmail.com>
2016-01-05 11:24:05 -0800

    updated gitignore

--- clean_data.bash
+++
@@ -1,8 +0,0 @@
-# remove random date in random columns
-sed -i.bak '/sep 20, 2008/d' ratings.tsv 
-
-# remove none ascii columns (chinese characters among others)
-perl -pi -e 's/[[:^ascii:]]//g' ratings.tsv
-
-#remove double qoutes
-sed -i.bak 's/\"//g' ratings.tsv
